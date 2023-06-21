df9c3da65e42ab1c100a6de73ab75e9ae09a8304
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2023-06-21 12:51:53 +0300

    Added gitlab.yml and fixed the parent plugin compatibility issue

--- admin/class-kbtfw-addon-admin.php
+++ admin/class-kbtfw-addon-admin.php
@@ -1,27 +1,29 @@
 <?php
 
-class BKB_kbtfw_Admin {
-    
+class BKB_kbtfw_Admin
+{
+
     protected static $instance = null;
-    
+
     protected $plugin_screen_hook_suffix = null;
-    
-    private function __construct() {
+
+    private function __construct()
+    {
 
         //@Description: First we need to check if KB Plugin & WooCommerce is activated or not. If not then we display a message and return false.
         //@Since: Version 1.0.5
-        
-        if( ! class_exists( 'BWL_KB_Manager' ) || ! class_exists( 'WooCommerce' ) || BKBKBTFW_PARENT_PLUGIN_INSTALLED_VERSION < '1.0.5' ) {
+
+        if (!class_exists('BWL_KB_Manager') || !class_exists('WooCommerce') || BKBKBTFW_PARENT_PLUGIN_INSTALLED_VERSION < '1.0.5') {
             add_action('admin_notices', array($this, 'kbtfw_version_update_admin_notice'));
             return false;
         }
-        
+
         // Start Plugin Admin Panel Code.
-        
+
         $plugin = BKB_kbtfw::get_instance();
         $this->plugin_slug = $plugin->get_plugin_slug();
         $post_types = 'product';
-    
+
         add_action('admin_init', array($this, 'kbtfw_cmb_framework'));
 
         add_action('admin_enqueue_scripts', array($this, 'bkb_kbtfw_admin_enqueue_scripts'));
@@ -40,7 +42,6 @@ class BKB_kbtfw_Admin {
 
         add_action('save_post', array($this, 'kbtfw_product_save_quick_edit_data'), 10, 2);
         add_action('wp_ajax_manage_wp_posts_using_bulk_edit_kbtfw', array($this, 'manage_wp_posts_using_bulk_edit_kbtfw'));
-        
     }
 
     /**
@@ -50,7 +51,8 @@ class BKB_kbtfw_Admin {
      *
      * @return    object    A single instance of this class.
      */
-    public static function get_instance() {
+    public static function get_instance()
+    {
 
         /*
          * @TODO :
@@ -68,17 +70,17 @@ class BKB_kbtfw_Admin {
 
         return self::$instance;
     }
-    
+
     //Version Manager:  Update Checking
 
-    public function kbtfw_version_update_admin_notice() {
+    public function kbtfw_version_update_admin_notice()
+    {
 
-       
-    echo '<div class="updated"><p>You need to download & install both '
+
+        echo '<div class="updated"><p>You need to download & install both '
             . '<b><a href="http://downloads.wordpress.org/plugin/woocommerce.zip" target="_blank">WooCommerce Plugin</a></b> && '
             . '<b><a href="https://1.envato.market/bkbm-wp" target="_blank">BWL Knowledge Base Manager Plugin</a></b> '
             . 'to use <b>KB Tab For WooCommerce - Knowledgebase Addon</b>. </p></div>';
-    
     }
 
     /**
@@ -86,16 +88,16 @@ class BKB_kbtfw_Admin {
      *
      * @since    1.0.0
      */
-    public function bkb_kbtfw_admin_enqueue_scripts( $hook ) {
-        
+    public function bkb_kbtfw_admin_enqueue_scripts($hook)
+    {
+
         // We only load this JS script in product add/edit page.
-        
-       $current_post_type = "";
+
+        $current_post_type = "";
 
         if (isset($_GET['post_type']) && $_GET['post_type'] == "product") {
 
             $current_post_type = "product";
-            
         } else if (isset($_GET['post']) && get_post_type($_GET['post']) === 'product') {
 
             $current_post_type = "product";
@@ -104,8 +106,8 @@ class BKB_kbtfw_Admin {
             $current_post_type = "";
         }
 
-        if ( $current_post_type == "product" ) {
-      
+        if ($current_post_type == "product") {
+
             wp_register_script('bkb-cmb-admin-main', BWL_KB_PLUGIN_DIR . 'includes/bkb-cmb-framework/admin/js/bkb_cmb.js', array('jquery', 'jquery-ui-core', 'jquery-ui-sortable'), false, false);
             wp_register_style('bkb-cmb-admin-style', BWL_KB_PLUGIN_DIR . 'includes/bkb-cmb-framework/admin/css/bkb_cmb.css', array(), false, 'all');
 
@@ -113,16 +115,15 @@ class BKB_kbtfw_Admin {
             wp_enqueue_style('bkb-cmb-admin-style');
 
             wp_enqueue_script($this->plugin_slug . '-admin-custom-script', plugins_url('assets/js/kbtfw-admin-scripts.js', __FILE__), array('jquery'), BKB_kbtfw::VERSION, TRUE);
-            
         } else {
-            
+
             return;
-            
         }
     }
 
-    function kbtfw_cmb_framework() {
-        
+    function kbtfw_cmb_framework()
+    {
+
         $args = array(
             'post_status' => 'publish',
             'post_type' => 'bwl_kb',
@@ -152,8 +153,8 @@ class BKB_kbtfw_Admin {
         endif;
 
         wp_reset_query();
-        
-         $cmb_bkb_woo_item_fields= array(
+
+        $cmb_bkb_woo_item_fields = array(
 
             'meta_box_id'           => 'cmb_bkb_woo_item_settings', // Unique id of meta box.
             'meta_box_heading'  => 'Knowledgebase Item Settings', // That text will be show in meta box head section.
@@ -161,49 +162,53 @@ class BKB_kbtfw_Admin {
             'context'                   => 'normal',
             'priority'                    => 'high',
             'fields'                       => array(
-                
-                                                        'bkb_woo_tab_hide_status'  => array(
-                                                                                    'title'      => __( 'Hide Knowledge Base Tab?', 'bkb-kbtfw'),
-                                                                                    'id'         => 'bkb_woo_tab_hide_status',
-                                                                                    'name'    => 'bkb_woo_tab_hide_status',
-                                                                                    'type'      => 'select',
-                                                                                    'value'     => array(
-                                                                                                            '1' => __('Yes', 'bkb-kbtfw'),
-                                                                                                            '2' => __('No', 'bkb-kbtfw')
-                                                                                                        ),
-                                                                                    'default_value' => 2,
-                                                                                    'class'      => 'widefat'
-                                                                                ),
-                                                        'kbftw_kb_post_ids'  => array(
-                                                                                    'title'      => __( 'Add Knowledgebase Items', 'bkb-kbtfw'),
-                                                                                    'id'         => 'kbftw_kb_post_ids',
-                                                                                    'name'    => 'kbftw_kb_post_ids',
-                                                                                    'type'      => 'repeatable_select',
-                                                                                    'value'     => '',
-                                                                                    'default_value' => $kbftw_kb_post_ids,
-                                                                                    'class'      => 'widefat',
-                                                                                    'btn_text'      => 'Add New KB',
-                                                                                    'label_text'      => 'KB Post',
-                                                                                    'field_type' => 'select'
-                                                                                ),
-                                                    )
+
+                'bkb_woo_tab_hide_status'  => array(
+                    'title'      => __('Hide Knowledge Base Tab?', 'bkb-kbtfw'),
+                    'id'         => 'bkb_woo_tab_hide_status',
+                    'name'    => 'bkb_woo_tab_hide_status',
+                    'type'      => 'select',
+                    'value'     => array(
+                        '1' => __('Yes', 'bkb-kbtfw'),
+                        '2' => __('No', 'bkb-kbtfw')
+                    ),
+                    'default_value' => 2,
+                    'class'      => 'widefat'
+                ),
+                'kbftw_kb_post_ids'  => array(
+                    'title'      => __('Add Knowledgebase Items', 'bkb-kbtfw'),
+                    'id'         => 'kbftw_kb_post_ids',
+                    'name'    => 'kbftw_kb_post_ids',
+                    'type'      => 'repeatable_select',
+                    'value'     => '',
+                    'default_value' => $kbftw_kb_post_ids,
+                    'class'      => 'widefat',
+                    'btn_text'      => 'Add New KB',
+                    'label_text'      => 'KB Post',
+                    'field_type' => 'select'
+                ),
+            )
         );
 
 
-        new BKB_Meta_Box( $cmb_bkb_woo_item_fields );
-        
+        // new BKB_Meta_Box( $cmb_bkb_woo_item_fields );
+
     }
 
-    function kbtfw_custom_column_header($columns) {
+    function kbtfw_custom_column_header($columns)
+    {
 
-        return array_merge($columns, array(
-            'kbftw_kb_post_ids' => __('Total <br />KBs', 'bkb-kbtfw'),
-            'bkb_woo_tab_hide_status' => __('KB Tab <br />Status', 'bkb-kbtfw')
-                )
+        return array_merge(
+            $columns,
+            array(
+                'kbftw_kb_post_ids' => __('Total <br />KBs', 'bkb-kbtfw'),
+                'bkb_woo_tab_hide_status' => __('KB Tab <br />Status', 'bkb-kbtfw')
+            )
         );
     }
 
-    function kbtfw_display_custom_column($column) {
+    function kbtfw_display_custom_column($column)
+    {
 
         // Add A Custom Image Size For Admin Panel.
 
@@ -213,18 +218,18 @@ class BKB_kbtfw_Admin {
 
             case 'kbftw_kb_post_ids':
 
-                $kbftw_kb_post_ids = ( int ) count( apply_filters('filter_kbtfwc_content_data', get_post_meta($post->ID, 'kbftw_kb_post_ids') ) ) ;
+                $kbftw_kb_post_ids = (int) count(apply_filters('filter_kbtfwc_content_data', get_post_meta($post->ID, 'kbftw_kb_post_ids')));
                 echo '<div id="kbftw_kb_post_ids-' . $post->ID . '" >&nbsp;' . $kbftw_kb_post_ids . '</div>';
 
                 break;
 
             case 'bkb_woo_tab_hide_status':
 
-                $bkb_woo_tab_hide_status = ( get_post_meta($post->ID, "bkb_woo_tab_hide_status", true) == "" ) ? "" : get_post_meta($post->ID, "bkb_woo_tab_hide_status", true);
+                $bkb_woo_tab_hide_status = (get_post_meta($post->ID, "bkb_woo_tab_hide_status", true) == "") ? "" : get_post_meta($post->ID, "bkb_woo_tab_hide_status", true);
 
                 // FAQ Display Status In Text.
 
-                $bkb_woo_tab_hide_status_in_text = ( $bkb_woo_tab_hide_status == 1 ) ? __("Hidden", "bkb-kbtfw") : __("Visible", "bkb-kbtfw");
+                $bkb_woo_tab_hide_status_in_text = ($bkb_woo_tab_hide_status == 1) ? __("Hidden", "bkb-kbtfw") : __("Visible", "bkb-kbtfw");
 
                 echo '<div id="bkb_woo_tab_hide_status-' . $post->ID . '" data-status_code="' . $bkb_woo_tab_hide_status . '" >' . $bkb_woo_tab_hide_status_in_text . '</div>';
 
@@ -234,7 +239,8 @@ class BKB_kbtfw_Admin {
 
     /* ------------------------------ Bulk & Quick Edit Section --------------------------------- */
 
-    function kbtfw_product_quick_edit_box($column_name, $post_type) {
+    function kbtfw_product_quick_edit_box($column_name, $post_type)
+    {
 
         global $post;
 
@@ -245,12 +251,12 @@ class BKB_kbtfw_Admin {
                 switch ($column_name) {
 
                     case 'bkb_woo_tab_hide_status':
-                        
+
                         $bkb_woo_tab_hide_status_val = get_post_meta($post->ID, "bkb_woo_tab_hide_status", true);
 
-                        $bkb_woo_tab_hide_status = (  $bkb_woo_tab_hide_status_val == "" ) ? "" : $bkb_woo_tab_hide_status_val;
-                        
-                        ?>
+                        $bkb_woo_tab_hide_status = ($bkb_woo_tab_hide_status_val == "") ? "" : $bkb_woo_tab_hide_status_val;
+
+?>
 
 
                         <fieldset class="inline-edit-col-left">
@@ -271,7 +277,7 @@ class BKB_kbtfw_Admin {
                         </fieldset>
 
 
-                        <?php
+                    <?php
                         break;
                 }
 
@@ -279,7 +285,8 @@ class BKB_kbtfw_Admin {
         }
     }
 
-    function kbtfw_product_save_quick_edit_data($post_id, $post) {
+    function kbtfw_product_save_quick_edit_data($post_id, $post)
+    {
 
         // pointless if $_POST is empty (this happens on bulk edit)
         if (empty($_POST))
@@ -320,7 +327,8 @@ class BKB_kbtfw_Admin {
         }
     }
 
-    function kbtfw_product_bulk_edit_box($column_name, $post_type) {
+    function kbtfw_product_bulk_edit_box($column_name, $post_type)
+    {
 
         global $post;
 
@@ -331,7 +339,7 @@ class BKB_kbtfw_Admin {
                 switch ($column_name) {
 
                     case 'bkb_woo_tab_hide_status':
-                        ?>
+                    ?>
                         <fieldset class="inline-edit-col-right">
                             <div class="inline-edit-col">
                                 <div class="inline-edit-group">
@@ -347,7 +355,7 @@ class BKB_kbtfw_Admin {
                             </div>
                         </fieldset>
 
-                        <?php
+<?php
                         break;
                 }
 
@@ -355,10 +363,11 @@ class BKB_kbtfw_Admin {
         }
     }
 
-    function manage_wp_posts_using_bulk_edit_kbtfw() {
+    function manage_wp_posts_using_bulk_edit_kbtfw()
+    {
 
         // we need the post IDs
-        $post_ids = ( isset($_POST['post_ids']) && !empty($_POST['post_ids']) ) ? $_POST['post_ids'] : NULL;
+        $post_ids = (isset($_POST['post_ids']) && !empty($_POST['post_ids'])) ? $_POST['post_ids'] : NULL;
 
         // if we have post IDs
         if (!empty($post_ids) && is_array($post_ids)) {
@@ -389,6 +398,4 @@ class BKB_kbtfw_Admin {
             }
         }
     }
-    
-
 }
