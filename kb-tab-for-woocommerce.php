16d08e8913a1eb1995af3cf6906069b32f5d631f
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2023-07-06 16:50:38 +0300

    update-version-1.1.1

--- kb-tab-for-woocommerce.php
+++ kb-tab-for-woocommerce.php
@@ -4,7 +4,7 @@
  * Plugin Name:    KB Tab For WooCommerce - Knowledgebase Addon
  * Plugin URI:       http://bit.ly/bkbm-woo
  * Description:      KB tab for woocommerce Addon allows you to convert you're knowledge base posts in to WooCommerce product Knowledgebase item with in a minute. You can add unlimited number of knowledge base post as product knowledgebase items and using drag drop feature sort them according to you're choice.
- * Version:           1.1.0
+ * Version:           1.1.1
  * Author:             Md Mahbub Alam Khan
  * Author URI:       http://codecanyon.net/user/xenioushk?ref=xenioushk
  * Text Domain:     bkb-kbtfw
@@ -17,8 +17,8 @@ if (!defined('WPINC')) {
 define('BKBKBTFW_PARENT_PLUGIN_INSTALLED_VERSION', get_option('bwl_kb_plugin_version'));
 define('BKBKBTFW_ADDON_PARENT_PLUGIN_TITLE', '<b>BWL Knowledge Base Manager Plugin</b> ');
 define('BKBKBTFW_ADDON_TITLE', '<b>KB Tab For WooCommerce</b>');
-define('BKBKBTFW_PARENT_PLUGIN_REQUIRED_VERSION', '1.0.9'); // change plugin required version in here.
-define('BKBKBTFW_ADDON_CURRENT_VERSION', '1.1.0'); // change plugin current version in here.
+define('BKBKBTFW_PARENT_PLUGIN_REQUIRED_VERSION', '1.4.2'); // change plugin required version in here.
+define('BKBKBTFW_ADDON_CURRENT_VERSION', '1.1.1'); // change plugin current version in here.
 define('BKBKBTFW_ADDON_PREFIX', 'bkb-kbtfw'); // Addon Data Prefix. It must be simmilar with $plugin slug (kb-tab-for-woocommerce\public\class-kbtfw-addon.php).
 
 define('BKBKBTFW_DIR', plugin_dir_path(__FILE__));
